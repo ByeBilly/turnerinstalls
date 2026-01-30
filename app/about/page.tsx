93131614355dcd82d6d37396598143615b03276c
ByeBilly <billiamglobal@gmail.com>
2026-01-31 06:08:52 +1100

    Update About Page Hero to use authentic 'brisbane views.jpg'

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,8 +18,8 @@ export default function About() {
       <section className="relative min-h-[60vh] flex items-center justify-center overflow-hidden">
         <div className="absolute inset-0 z-0">
           <Image
-            src="/images/brisbane_skyline.png"
-            alt="Turner Installs Team"
+            src="/installspics/finished/brisbane views.jpg"
+            alt="Brisbane Views - Turner Installs Projects"
             fill
             className="object-cover"
             priority
